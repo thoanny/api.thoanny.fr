88d023cece2146b16fa55d2d9e3236e74c8c07e7
Anthony Destenay <anthony.destenay@gmail.com>
2025-01-28 18:21:00 +0100

    Added legal notice

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,13 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('page/home.html.twig');
+    }
+
+    #[Route('/legal-notice', name: 'app_legal_notice')]
+    public function legalNotice(): Response
+    {
+        // https://fr.orson.io
+        return $this->render('page/legal-notice.html.twig');
     }
 }
