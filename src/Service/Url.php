2cf6ec89745ea80a809f59354f580e3aa0e5a8fb
Anthony Destenay <anthony.destenay@gmail.com>
2025-02-20 10:02:39 +0100

    Fixed URL service if no path

--- src/Service/Url.php
+++ src/Service/Url.php
@@ -5,6 +5,7 @@ class Url
     public function getCleanUrl(string $url): string
     {
         $parts = parse_url($url);
-        return "{$parts['scheme']}://{$parts['host']}{$parts['path']}";
+        $path = $parts['path']??'';
+        return "{$parts['scheme']}://{$parts['host']}$path";
     }
 }
