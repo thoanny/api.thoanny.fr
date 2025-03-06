2ea4878c102bd02593093ad5e63a0e171b0711a6
Anthony Destenay <anthony.destenay@gmail.com>
2025-03-06 17:19:15 +0100

    Added feed RSS description

--- src/Form/Admin/PressReview/PostType.php
+++ src/Form/Admin/PressReview/PostType.php
@@ -4,10 +4,8 @@ namespace App\Form\Admin\PressReview;
 
 use App\Entity\PressReview\Category;
 use App\Entity\PressReview\Post;
-use App\Form\Type\EditorJsType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -23,7 +21,7 @@ class PostType extends AbstractType
             ->add('published_at', null, [
                 'widget' => 'single_text',
             ])
-            ->add('description', EditorJsType::class)
+            ->add('description')
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
